b7b23103c6d79c3e2573d9171b47e1d6bd366710
thirumurugan7 <thiru82003@gmail.com>
2023-01-29 20:04:20 +0530

    alchemy univeristy course day-01

---
+++ index.js
@@ -0,0 +1,18 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+const a = "apple";
+const b = "banana";
+
+const aBytes = utf8ToBytes(a);
+const bBytes = utf8ToBytes(b);
+console.log(`abytes: ${aBytes}`);
+console.log(`bbytes: ${bBytes}`);
+const aHash = sha256(aBytes);
+console.log(`aHash:${aHash} `);
+
+const bHash = sha256(bBytes);
+console.log(`bHash:${bHash} `);
+console.log(toHex(aHash) === toHex(aHash)); // true
+console.log(toHex(aHash) === toHex(bHash)); // false
